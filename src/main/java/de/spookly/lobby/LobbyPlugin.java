ae94d19348e083f2123f787a4d5c4cdfd1b5b9fe
DaaanielTV <daniel.rosemann10@gmail.com>
2025-04-17 17:14:26 +0200

    rewrite without spooky dependencies

--- src/main/java/de/spookly/lobby/LobbyPlugin.java
+++ src/main/java/de/spookly/lobby/LobbyPlugin.java
@@ -4,9 +4,6 @@ import java.io.File;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-import de.nehlen.spookly.Spookly;
-import de.nehlen.spookly.configuration.ConfigurationWrapper;
-import de.nehlen.spookly.plugin.SpooklyPlugin;
 import lombok.Getter;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.translation.GlobalTranslator;
@@ -20,105 +17,133 @@ import de.spookly.lobby.commands.SetspawnCommand;
 import de.spookly.lobby.listener.*;
 import de.spookly.lobby.manager.ScoreboardManager;
 import de.spookly.lobby.sidebar.SidebarCache;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
 
 @Getter
-public class LobbyPlugin extends SpooklyPlugin {
+public class LobbyPlugin extends JavaPlugin {
 
-	@Getter
-	private static LobbyPlugin instance;
+    @Getter
+    private static LobbyPlugin instance;
 
-	@Deprecated(forRemoval = true)
-	public static LobbyPlugin getLobby() {
-		return instance;
-	}
+    private FileConfiguration generalConfig;
+    private FileConfiguration npcConfig;
+    private FileConfiguration locationConfig;
 
-	private ConfigurationWrapper generalConfig;
-	private ConfigurationWrapper npcConfig;
-	private ConfigurationWrapper locationConfig;
+    private File generalConfigFile;
+    private File npcConfigFile;
+    private File locationConfigFile;
 
+    private SidebarCache sidebarCache;
+    private ScoreboardManager scoreboardManager;
 
-	private SidebarCache sidebarCache;
-	private ScoreboardManager scoreboardManager;
+    private InteractListener interactListener;
+    private PlayerJoinListener playerJoinListener;
+    private PlayerMoveListener playerMoveListener;
+    private PlayerQuitListener playerQuitListener;
+    private AsyncChatListener asyncChatListener;
+    private de.spookly.lobby.listener.Listener listener;
 
-	private InteractListener interactListener;
-	private PlayerJoinListener playerJoinListener;
-	private PlayerMoveListener playerMoveListener;
-	private PlayerQuitListener playerQuitListener;
-	private AsyncChatListener asyncChatListener;
-	private Listener listener;
+    private SetspawnCommand setspawnCommand;
+    private BuildCommand buildCommand;
+    private FlyCommand flyCommand;
 
-	private SetspawnCommand setspawnCommand;
-	private BuildCommand buildCommand;
-	private FlyCommand flyCommand;
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
 
-	@Override
-	protected void load() {
-		instance = this;
-	}
+    @Override
+    public void onEnable() {
+        // Create config files
+        createConfigs();
+        getLogger().info("Loaded configuration files");
 
-	@Override
-	protected void enable() {
-		this.generalConfig = Spookly.getServer().createConfiguration(new File(getDataFolder(), "general_settings.yml"));
-		this.locationConfig = Spookly.getServer().createConfiguration(new File(getDataFolder(), "location_settings.yml"));
-		this.npcConfig = Spookly.getServer().createConfiguration(new File(getDataFolder(), "npc_settings.yml"));
-		getLogger().info("Loaded configuration files");
+        this.sidebarCache = new SidebarCache();
+        this.scoreboardManager = new ScoreboardManager(this);
+        getLogger().info("Loaded managers");
 
-//        this.databaseLib = Spookly.getServer().getConnection();
-//        this.cosmeticProfileSql = new CosmeticProfileSql(databaseLib);
-		getLogger().info("Loaded Database");
+        this.interactListener = new InteractListener(this);
+        this.playerJoinListener = new PlayerJoinListener(this);
+        this.playerMoveListener = new PlayerMoveListener(this);
+        this.playerQuitListener = new PlayerQuitListener(this);
+        this.asyncChatListener = new AsyncChatListener();
+        this.listener = new de.spookly.lobby.listener.Listener(this);
+        this.setspawnCommand = new SetspawnCommand(this);
+        this.buildCommand = new BuildCommand(this);
+        this.flyCommand = new FlyCommand();
+        getLogger().info("Loaded listener / commands");
 
-		this.sidebarCache = new SidebarCache();
-		this.scoreboardManager = new ScoreboardManager(this);
-//        this.factoryUserCosmetics = new FactoryUserCosmetics(this);
-		getLogger().info("Loaded managers");
+        registerEvents();
+        registerCommands();
 
-		this.interactListener = new InteractListener(this);
-		this.playerJoinListener = new PlayerJoinListener(this);
-		this.playerMoveListener = new PlayerMoveListener(this);
-		this.playerQuitListener = new PlayerQuitListener(this);
-		this.asyncChatListener = new AsyncChatListener();
-		this.listener = new Listener(this);
-		this.setspawnCommand = new SetspawnCommand(this);
-		this.buildCommand = new BuildCommand(this);
-		this.flyCommand = new FlyCommand();
-		getLogger().info("Loaded listener / commands");
+        Bukkit.getScheduler().runTaskLater(this, this::postStartup, 1L);
+    }
 
-		registerEvent(listener);
-		registerEvent(interactListener);
-		registerEvent(playerJoinListener);
-		registerEvent(playerQuitListener);
-		registerEvent(playerMoveListener);
-		registerEvent(asyncChatListener);
-		registerEvent(new MenuFunctionListener());
+    private void createConfigs() {
+        saveDefaultConfig();
+        
+        generalConfigFile = new File(getDataFolder(), "general_settings.yml");
+        npcConfigFile = new File(getDataFolder(), "npc_settings.yml");
+        locationConfigFile = new File(getDataFolder(), "location_settings.yml");
 
-		registerCommandOnly("build", buildCommand);
-		registerCommandOnly("fly", flyCommand);
-		registerCommandOnly("setspawn", setspawnCommand);
+        if (!generalConfigFile.exists()) saveResource("general_settings.yml", false);
+        if (!npcConfigFile.exists()) saveResource("npc_settings.yml", false);
+        if (!locationConfigFile.exists()) saveResource("location_settings.yml", false);
 
-		Bukkit.getScheduler().runTaskLater(this, this::postStartup, 1L);
-	}
+        generalConfig = YamlConfiguration.loadConfiguration(generalConfigFile);
+        npcConfig = YamlConfiguration.loadConfiguration(npcConfigFile);
+        locationConfig = YamlConfiguration.loadConfiguration(locationConfigFile);
+    }
 
-	@Override
-	protected void disable() {
+    private void registerEvents() {
+        registerEvent(listener);
+        registerEvent(interactListener);
+        registerEvent(playerJoinListener);
+        registerEvent(playerQuitListener);
+        registerEvent(playerMoveListener);
+        registerEvent(asyncChatListener);
+        registerEvent(new MenuFunctionListener());
+    }
 
-	}
+    private void registerEvent(Listener listener) {
+        Bukkit.getPluginManager().registerEvents(listener, this);
+    }
 
-	@Override
-	protected void postStartup() {
-//        this.chestTraderHandler = new ChestTraderHandler(this);
-		registerTranslatables();
-	}
+    private void registerCommands() {
+        getCommand("build").setExecutor(buildCommand);
+        getCommand("fly").setExecutor(flyCommand);
+        getCommand("setspawn").setExecutor(setspawnCommand);
+    }
 
-	private void registerTranslatables() {
-		TranslationRegistry registry = TranslationRegistry.create(Key.key("spookly_lobby:value"));
-		ResourceBundle bundleUS = ResourceBundle.getBundle("SpooklyLobby.Translations", Locale.US, UTF8ResourceBundleControl.get());
-		ResourceBundle bundleDE = ResourceBundle.getBundle("SpooklyLobby.Translations", Locale.GERMANY, UTF8ResourceBundleControl.get());
+    @Override
+    public void onDisable() {
+        try {
+            generalConfig.save(generalConfigFile);
+            npcConfig.save(npcConfigFile);
+            locationConfig.save(locationConfigFile);
+        } catch (Exception e) {
+            getLogger().severe("Could not save config files!");
+            e.printStackTrace();
+        }
+    }
 
-		registry.registerAll(Locale.US, bundleUS, true);
-		registry.registerAll(Locale.GERMAN, bundleDE, true);
+    protected void postStartup() {
+        registerTranslatables();
+    }
 
-		GlobalTranslator.translator().addSource(registry);
-	}
+    private void registerTranslatables() {
+        TranslationRegistry registry = TranslationRegistry.create(Key.key("spookly_lobby:value"));
+        ResourceBundle bundleUS = ResourceBundle.getBundle("SpooklyLobby.Translations", Locale.US, UTF8ResourceBundleControl.get());
+        ResourceBundle bundleDE = ResourceBundle.getBundle("SpooklyLobby.Translations", Locale.GERMANY, UTF8ResourceBundleControl.get());
+
+        registry.registerAll(Locale.US, bundleUS, true);
+        registry.registerAll(Locale.GERMAN, bundleDE, true);
+
+        GlobalTranslator.translator().addSource(registry);
+    }
 }
